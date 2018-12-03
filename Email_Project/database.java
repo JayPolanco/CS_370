65c912dfd2107bfd586b90555440c49f69b9a916
JayPolanco <44456195+JayPolanco@users.noreply.github.com>
2018-12-03 18:24:57 -0500

    Update database.java

--- Email_Project/database.java
+++ Email_Project/database.java
@@ -1,3 +1,6 @@
+/*This class contains all of the database methods, including connectivity methods and the
+the methods used to send and retreive emails. As well as methods used for storing new accounts in the DB
+*/
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -10,7 +13,7 @@ import javax.swing.table.DefaultTableModel;
 public class database {
 	public static Connection myConn;
 	public static LinkedList<String> ListOfUsers = new LinkedList<String>();
-	
+	//public static LinkedList<email> ListOfEmails = new LinkedList<email>();
 	private static User currentUser;
 	private String nextUserInTable;
 	//Gets connection to a database with the name email
@@ -24,8 +27,8 @@ public class database {
 		
 		try {
 				
-			//connect to database, xxxxxxx represents the poassword for the DB
-			myConn = DriverManager.getConnection(url, "XXX", "XXXXX");
+			//connect to database, xxxxxxx represents the password for the DB
+			myConn = DriverManager.getConnection(url, "root", "XXXXXXXXX");
 			return myConn;
 		}
 		catch (SQLException ex) {
@@ -33,7 +36,7 @@ public class database {
 		}
 
 	}
-	
+	//just to test if I connected
 	public static LinkedList<String> getAllUser() {
 		String sql = "SELECT * FROM email.users";
 		try {
@@ -41,7 +44,7 @@ public class database {
 			rs = stmt.executeQuery();
 			
 			while(rs.next()){
-				
+				//Adds the users to a list, just so we can see all the users(for testing)
 				ListOfUsers.add(rs.getString("username") + rs.getString("password"));;
 			}
 			return ListOfUsers;
@@ -72,7 +75,8 @@ public class database {
 			
 			stmt.executeUpdate();
 			
-		
+			
+			//System.out.print("User entered into DB");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -111,9 +115,10 @@ public class database {
 					subject = rs.getString("subject");
 					timestamp = rs.getString("time");
 					primeKey = rs.getInt("emailNum");
-					body = rs.getString("body");                
+					body = rs.getString("body");                //*******KARL********
 					
-					Homepage.add_row(model_box, fromEmail, subject, timestamp, primeKey, body); 
+					//System.out.println("\n HOMEPAGE.ADD_ROW BEING RUN Inbox"+fromEmail);
+					Homepage.add_row(model_box, fromEmail, subject, timestamp, primeKey, body); //*******KARL********
 					
 				}
 			} catch (SQLException e) {
@@ -134,7 +139,7 @@ public class database {
 			String sql = "SELECT * FROM email.`" + u.getUserName() + "`" +
 					" WHERE location = '" + locationCHAR +
 					"' ORDER BY emailNum ASC";
-			String toEmail, subject, timestamp, body; 
+			String toEmail, subject, timestamp, body;                
 			int primeKey;
 			
 			try {
@@ -147,7 +152,7 @@ public class database {
 					subject = rs.getString("subject");
 					timestamp = rs.getString("time");
 					primeKey = rs.getInt("emailNum");
-					body = rs.getString("body");
+					body = rs.getString("body");	                 
 					
 					Homepage.add_row(model_box, toEmail , subject, timestamp, primeKey, body); 
 					
@@ -188,8 +193,199 @@ public class database {
 			//e.printStackTrace();
 		}
 	}
-		
 	}
 	
 	public static void addEmail(String to, String from, String subject, String body, String location, String needTimeStamp) {
 		
+		try {
+			String insert = "Insert into email." +"`" + from +
+							"`" + " (`from`, `to`, subject, body, location, timestamp, time)" + 
+					"VALUES(?,?,?,?,?,?, CURRENT_TIME)";                            
+			stmt = Connect().prepareStatement(insert);
+			
+			//filling in question marks
+			stmt.setString(1, from);
+			stmt.setString(2, to);
+			stmt.setString(3, subject);
+			stmt.setString(4, body);
+			stmt.setString(5, location);
+			stmt.setString(6, needTimeStamp);
+			
+			//execute statement
+			stmt.executeUpdate();
+			
+			
+		} catch (SQLException e) {
+			//e.printStackTrace();
+		}
+		finally {
+			try {
+				Connect().close();
+				stmt.close();
+			} catch (SQLException e) {
+			}
+			
+		}
+	}//No PK Parameter, then it is a new email and it will need one for the DB
+	
+	
+	public static void addEmail(int pk, String to, String from, String subject, String body, String location, String needTimeStamp) {
+
+		String up = "UPDATE email.`" + from + "`" +
+					" SET `From` = '" + from + "'," +
+					   " `to` = '" + to + "'," +
+					   " subject = '" + subject + "'," +
+					   " body = '" + body + "'," +
+					   " location = '" + location + "'," +
+					   " timestamp = '" + needTimeStamp + "'," +
+					   " time = CURRENT_TIMESTAMP" + 
+				" WHERE emailNum = " + pk;
+	try {
+		update = Connect().prepareStatement(up);
+		update.executeUpdate();
+		
+		
+	} catch (SQLException ex) {
+		//ex.printStackTrace();
+	}
+	finally {
+		try {
+			Connect().close();
+			update.close();
+		} catch (SQLException e) {
+			//e.printStackTrace();
+		}
+		
+	}
+	
+		
+		
+	}//PK is provided, update the email
+	
+	//INSERTS THE RECEIEVED MESSAGE INTO THE DB OF RECEIVER
+	public static void addEmailToSent(String to, String from, String subject, String body, String location, String needTimeStamp) {
+		try {
+			String insert = "Insert into email." +"`" + to +
+							"`" + " (`from`, `to`, subject, body, location, timestamp, time)" + 
+					"VALUES(?,?,?,?,?,?, CURRENT_TIMESTAMP)";
+			stmt = Connect().prepareStatement(insert);
+			
+			//filling in question marks
+			stmt.setString(1, from);
+			stmt.setString(2, to);
+			stmt.setString(3, subject);
+			stmt.setString(4, body);
+			stmt.setString(5, location);
+			stmt.setString(6, needTimeStamp);
+			
+			//execute statement
+			stmt.executeUpdate();
+			
+			
+			
+			//System.out.print("Message added to DB");
+		} catch (SQLException e) {
+			//e.printStackTrace();
+		}
+		finally {
+			try {
+				Connect().close();
+				stmt.close();
+			} catch (SQLException e) {
+				//e.printStackTrace();
+			}
+			
+		}
+	
+	}
+	
+	public static String getFrom(int pk, String TableName) {
+		String getFrom = "SELECT * FROM email.`" + TableName + "`" +
+				" WHERE emailNum =" + pk;
+		String from;		
+		try {
+			stmt = Connect().prepareStatement(getFrom);
+			rs = stmt.executeQuery();
+				
+			from = rs.getString("From");
+			
+			Connect().close();
+			stmt.close();
+			rs.close();
+			
+			return from;
+		} catch (SQLException e) {
+			//e.printStackTrace();
+		}
+		return null;		
+		
+	}
+	
+	public static String getTo(int pk, String TableName) {
+		String getTo = "SELECT * FROM email.`" + TableName + "`" +
+				" WHERE emailNum = " + pk;
+		String to;		
+		try {
+			stmt = Connect().prepareStatement(getTo);
+			rs = stmt.executeQuery();
+			
+			to = rs.getString("to");
+			
+			Connect().close();
+			stmt.close();
+			rs.close();
+			
+			return to;
+		} catch (SQLException e) {
+		}
+		return null;
+	}
+	
+	public static String getSubject(int pk, String TableName) {
+		String getSub = "SELECT * FROM email.`" + TableName + "`" +
+				" WHERE emailNum = " + pk;
+		String sub;		
+		try {
+			stmt = Connect().prepareStatement(getSub);
+			rs = stmt.executeQuery();
+			
+		    sub = rs.getString("subject");
+			
+			Connect().close();
+			stmt.close();
+			rs.close();
+			
+			return sub;
+		} catch (SQLException e) {
+			//e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static String getDateTime(int pk, String TableName) {
+		String getTime = "SELECT * FROM email.`" + TableName + "`" +
+				" WHERE emailNum = " + pk;
+		String DateTime;		
+		try {
+			stmt = Connect().prepareStatement(getTime);
+			rs = stmt.executeQuery();
+			
+			//if(rs.next())
+			DateTime = rs.getString("time");
+			
+			Connect().close();
+			stmt.close();
+			rs.close();
+			
+			return DateTime;
+		} catch (SQLException e) {
+			//e.printStackTrace();
+		}
+		return null;
+	}
+
+}
+
+
+
+	
