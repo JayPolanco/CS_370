0b573c20f32d6c3abe9f07bd1f26e6b94cc0c25b
JayPolanco <44456195+JayPolanco@users.noreply.github.com>
2018-12-03 18:09:19 -0500

    Add files via upload

---
+++ MovieShowings/main.java
@@ -0,0 +1,12 @@
+/*Project goal: To create a movie showings GUI which allows users to view
+   the cinemas and times movies are playing. Also an optional admin can 
+   add new movies/showings/cinemas as well as delete current showings/movies and cinemas*/
+public class main {
+
+	public static void main(String[] args) {
+		
+		movieGUI m = new movieGUI();
+				
+	}
+
+}
